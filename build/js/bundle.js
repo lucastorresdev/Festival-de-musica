28b4f77fb60d7a7db7947725682808230d3cc065
Lucas <lucasltorres1997@gmail.com>
2021-09-29 10:49:10 -0300

    Página terminada

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -1,4 +1,41 @@
-console.log('LISTO!!')
+document.addEventListener('DOMContentLoaded', function(){
+    scrollNav()
+    navegacionFija();
+});
+
+function navegacionFija(){
+
+    const barra = document.querySelector('.header');
+
+    //Registrar el intersection observer
+
+    const observer = new IntersectionObserver( function(entries){
+       if(entries[0].isIntersecting){
+           barra.classList.remove('fijo');
+       }else{
+           barra.classList.add('fijo')
+       }
+    });
+
+    //Elemento observar
+
+    observer.observe(document.querySelector('.sobre-festival'));
+}
+
+function scrollNav(){
+    const enlaces = document.querySelectorAll('.navegacion-principal a');
+
+    enlaces.forEach( function(enlace){
+        enlace.addEventListener('click', function(e){
+            e.preventDefault();
+
+            const seccion = document.querySelector(e.target.attributes.href.value);
+            seccion.scrollIntoView({
+                behavior:'smooth',
+            });
+        });
+    });
+}
 document.addEventListener('DOMContentLoaded', function(){
 crearGaleria();
 });
